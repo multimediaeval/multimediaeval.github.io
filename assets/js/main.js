a517cdbbc0c6a0a38d3e3d9713e30d99e4f62cf6
ricoms <ricardosavii@gmail.com>
2020-05-25 23:59:35 -0300

    using materialize as main style setup

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Floating Action Button
+const elemsBtns = document.querySelectorAll(".fixed-action-btn");
+const floatingBtn = M.FloatingActionButton.init(elemsBtns, {
+    direction: "left",
+    hoverEnabled: false
+});
+
+// Navbar
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
+});
+const elemsSidenav = document.querySelectorAll(".sidenav");
+const instancesSidenav = M.Sidenav.init(elemsSidenav, {
+    edge: "left"
+})
